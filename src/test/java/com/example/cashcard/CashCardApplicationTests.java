77e02da28a9c31a265ca39eeb3566a1aa3d8eab7
richardmachage <rchacha261@gmail.com>
2024-05-05 03:37:01 +0300

    cash card object

--- src/test/java/com/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashCardApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.cashcard;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,7 +21,10 @@ class CashCardApplicationTests {
     void shouldReturnACashCardWhenDataIsSaved(){
         ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isNotNull();
     }
 
 
